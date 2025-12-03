1a8b8f78af7607f8eff44606d58b847a6a876297
Lohar-anil-01 <anilatsas01@gmail.com>
2025-12-03 17:33:50 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn React with <a href="https://chaicode.com/">Chaicode.com</a></h1>
+      <h2>Welcome to the React tutorial!</h2>
+    </>
+  )
+}
+
+export default App
