6e37e5ace1cb7d76002e2ec3c380937e5de7b520
Lohar-anil-01 <anilatsas01@gmail.com>
2025-12-03 17:56:31 +0530

    Changed Sub heading

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1>Learn React with <a href="https://chaicode.com/">Chaicode.com</a></h1>
-      <h2>Welcome to the React tutorial!</h2>
+      <h2>Welcome to the React Chai!</h2>
     </>
   )
 }
